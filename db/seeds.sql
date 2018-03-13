1938434741905022b35e9ca9e2dad6dea9024cce
James Rhee <jamesrhee@james-mbp.attlocal.net>
2018-03-13 16:47:26 -0700

    Eat da Burger Application

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quadruple-stacked Jalapeno Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern-Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Barbeque Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portabello Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rum Splash Burger', true);
